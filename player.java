d753bd250bfc3c2a6e510cc4d1ccb8231d17ad1d
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-19 14:24:26 -0500

    Update player.java

--- player.java
+++ player.java
@@ -1,1 +1,15 @@
-public player
+public class player{
+  private String name;
+  private String team;
+  private boolean playing;
+  public player{
+    this.name  = name;
+    this.team = team;
+  }
+  public String toString(){
+    return this.name;
+  }
+  public String getTeam(){
+    return this.team
+  }
+  
