0c6eda2cd7ee776c9dab09487064459384770001
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-19 14:43:23 -0500

    Update midfield.java

--- midfield.java
+++ midfield.java
@@ -1,1 +1,19 @@
-
+public class midfield extends player{
+  private int speed, pas, sho;
+  public midfield(String n, String t, int pac, int p, int s){
+    super(n, t);
+    pac = p;
+    pas = p;
+    speed = s;
+  }
+  public getsho(){
+    return this.sho;
+  }
+  public getpas(){
+    return this.pas;
+  }
+  public getdef(){
+    return this.def;
+  }
+  
+}
