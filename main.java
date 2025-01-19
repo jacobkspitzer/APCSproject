e8fa931d10ebe169c3889628156adbda1f87459a
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-19 15:26:51 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,1 +1,21 @@
+public class main{
+  public static void main(String[] args){
+    barcamids = new midfield[5];
+    barcafors = new forward[5];
+    barcadef = new defender[5];
+    barcateam = new player[6];
+    
+    barcamids[0] = new midfield("Iniesta", "Barca", 85, 90, 70);
+    barcamids[1] = new midfield("Xavi", "Barca", 80, 95, 75);
+    barcamids[2] = new midfield("Busquets", "Barca", 70, 90, 85);
+    barcamids[3] = new midfield("Rakitic", "Barca", 90, 80, 80);
+    barcamids[4] = new midfield("Pedri", "Barca", 83, 83, 83);
 
+    barcafors[0] = new forward("Messi", "Barca", 99, 99, 99);
+    barcafors[1] = new forward("Neymar", "Barca", 95, 80, 85);
+    barcafors[2] = new forward("Ronaldinho", "Barca", 95, 80, 85);
+    
+    
+      
+  }
+}
