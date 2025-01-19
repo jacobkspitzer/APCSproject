98285f7f7614c38c4c79e5c9831b5663001c98f8
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-19 14:28:30 -0500

    Update defender.java

--- defender.java
+++ defender.java
@@ -1,6 +1,17 @@
 public class defender{
-  public int def, pas, sho
+  private int def, pas, sho
   public defender(d, p, s){
-    
+    def = d;
+    pas = p;
+    sho = s;
+  }
+  public getsho(){
+    return this.sho;
+  }
+  public getpas(){
+    return this.pas;
+  }
+  public getdef(){
+    return this.getdef;
   }
 }
