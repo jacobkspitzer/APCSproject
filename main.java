dc498fb24078181512e2b9367dc76ed779cce04b
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-19 23:21:17 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,13 @@
 import java.util.Scanner;
+import java.util.Random;
+
 public class main{
   public static void main(String[] args){
     barcamids = new midfield[5];
     barcafors = new forward[5];
     barcadef = new defender[5];
     barcateam = new player[6];
+
     
     barcamids[0] = new midfield("Iniesta", "Barca", 90, 90, 80);
     barcamids[1] = new midfield("Xavi", "Barca", 80, 95, 85);
@@ -21,7 +24,7 @@ public class main{
     barcadefs[0] = new defender("Puyol", "Barca", 85, 85, 85);
     barcadefs[1] = new defender("Pique", "Barca", 80, 95, 80);
     barcadefs[2] = new defender("Araujo", "Barca", 85, 80, 95);
-    barcadefs[3] = new defender("Alves", "Barca", 95, 85, 80);
+    barcadefs[3] = new defender("Alves", "Barca", 95, 90, 80);
     barcadefs[4] = new defender("Alba", "Barca, 90, 85, 85);
 
     Scanner myscanner = new Scanner(System.in); 
@@ -34,7 +37,62 @@ public class main{
     System.out.println(barcateam[0].toString() + " is a good choice, pick another from the list the same way");
     int seconddef = scanner.nextInt();
     barcateam[1] = barcadefs[firstdef - 1];
-    System.out.println("
+    System.out.println("Nice, now pick a midfielder from this list: " + barcamids.toString());
+    int firstmid = scanner.nextInt();
+    barcateam[2] = barcamids[firstmid - 1];
+    System.out.println("Now pick another");
+    int secondmid = scanner.nextInt();
+    barcateam[3] = barcamids[secondmid - 1];
+    System.out.println("Good picks boss, but now you need an attack, pick two from this list: " barcafors.toString());
+    int firstforward = scanner.nextInt();
+    barcateam[4] = barcafors[firstforward - 1];
+    System.out.println("We'll need one more forward");
+    int secondforward = scanner.nextInt();
+    barcateam[5] = barcafors[firstforward - 1];
+    System.out.println("Well done boss. We will field a lineup of " + barcateam.toString() + " against Real tomorrow. I like our chances");
+
+    Madridteam = new player[6]
+    Madridteam[0] = new defender("Pepe", "Madrid", 90, 80, 85);
+    Madridteam[1] = new defender("Ramos", "Madrid", 85, 80, 90);
+    Madridteam[2] = new midfield("Modric", "Madrid", 80, 85, 90);
+    Madridteam[3] = new midfield("Kroos", "Madrid", 85, 90, 80);
+    Madridteam[4] = new forward("Ronaldo", "Madrid", 99, 99, 99);
+    Madridteam[5] = new forward("Bale", "Madrid", 85, 80, 95);
+    
+    Thread.sleep(2000);
+
+    int minute = 0;
+    int barcascore = 0;
+    int madridscore = 0;
+
+    
+    int barcapossesion = 0;
+    for(player p in barcateam){
+      barcapossesion += p.getpas();
+    }
+
+    int madridpossesion = 0;
+    for(player p in madridteam){
+      madridpossesion += p.getpas();
+    }
+    
+    int barcadef = 0;
+    
+    
+    System.out.println("The next day, at the Camp Nou...");
+    Thread.sleep(1000);
+    
+    
+
+    System.out.println("Commentator: On a warm night here in Catalonia, the very finest footballers the world has to offer have assembled to play out this edition of the greatest rivalry in sporting history. 
+      This is El Clasico, a battle not between two sets of footballers, but a battle between two footballing traditions, between two cities, and between two cultures. The intricate, passing football of barcalona,
+      against the lethal rapid counter attacks of Real Madrid. The home grown talents of Barca, against the Galactico Superstars of Madrid. The humble Messi against the arrogant Ronaldo. The stakes could not be
+      higher tonight. Only time will tell which side will emerge triumphant")
+      Thread.sleep(5000);
+    System.out.println("0': Barcelona kick off and we're underway. They have a spell of early possesion but lose it trying to go
+      
+    
+    
     
     
     
