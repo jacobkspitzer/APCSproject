f61bc1f64133f06a31ac7c728d6a3a30b1bd9469
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-19 23:42:23 -0500

    Update main.java

--- main.java
+++ main.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class main{
+  public static int getRandomNumber(int min, int max) {
+    return (int) ((Math.random() * (max - min)) + min);
+  }
   public static void main(String[] args){
     barcamids = new midfield[5];
     barcafors = new forward[5];
@@ -43,12 +46,10 @@ public class main{
     System.out.println("Now pick another");
     int secondmid = scanner.nextInt();
     barcateam[3] = barcamids[secondmid - 1];
-    System.out.println("Good picks boss, but now you need an attack, pick two from this list: " barcafors.toString());
+    System.out.println("Good picks boss, but now you need an attack. Of course Messi starts, now pick one from this list: " barcafors.toString());
     int firstforward = scanner.nextInt();
     barcateam[4] = barcafors[firstforward - 1];
-    System.out.println("We'll need one more forward");
-    int secondforward = scanner.nextInt();
-    barcateam[5] = barcafors[firstforward - 1];
+    barcateam[5] = barcafors[0];
     System.out.println("Well done boss. We will field a lineup of " + barcateam.toString() + " against Real tomorrow. I like our chances");
 
     Madridteam = new player[6]
@@ -61,7 +62,6 @@ public class main{
     
     Thread.sleep(2000);
 
-    int minute = 0;
     int barcascore = 0;
     int madridscore = 0;
 
@@ -77,7 +77,13 @@ public class main{
     }
     
     int barcadef = 0;
-    
+    for(int x = 0; x ++; x<4){
+      barcadef += barcateam[x].getdef();
+    }
+    int madriddef = 0;
+    for(int x = 0; x ++; x<4){
+      madriddef += madridteam[x].getdef();
+    }
     
     System.out.println("The next day, at the Camp Nou...");
     Thread.sleep(1000);
@@ -89,7 +95,18 @@ public class main{
       against the lethal rapid counter attacks of Real Madrid. The home grown talents of Barca, against the Galactico Superstars of Madrid. The humble Messi against the arrogant Ronaldo. The stakes could not be
       higher tonight. Only time will tell which side will emerge triumphant")
       Thread.sleep(5000);
-    System.out.println("0': Barcelona kick off and we're underway. They have a spell of early possesion but lose it trying to go
+    System.out.println("0': Barcelona kick off and we're underway. They have a spell of early possesion but lose it trying to go long looking for + " barcaplayers[6] + " Madrid tries to break quickly with " + 
+                      madridplayers[5] + "but halted in their tracks by" + barcaplayers[3] + ". Both sides look promising in the opening exchanges, hopefully the match continues like this.);
+
+    for(int x = 0; x <= 8; x ++){
+      minute = 5*x + getRandomNumber(0, 4);
+      String message = 
+      if( getRandomNumber(0, barcapossesion+madridpossesion) <= barcapossesion){
+        if (getRandomNumber(0, 100) <= 66){
+          
+        }
+      }
+    }
       
     
     
