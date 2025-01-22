1476bbb3dc800962df8de3a53052561311704853
jacobkspitzer <jacobkspitzer@gmail.com>
2025-01-22 11:23:14 -0500

    Update forward.java

--- forward.java
+++ forward.java
@@ -6,13 +6,13 @@ public class forward extends player{
     pas = p;
     speed = s;
   }
-  public getsho(){
+  public int getsho(){
     return this.sho;
   }
-  public getpas(){
+  public int getpas(){
     return this.pas;
   }
-  public getpac(){
+  public int getpac(){
     return this.speed;
   }
   
